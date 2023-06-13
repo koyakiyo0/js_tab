a740634755bdd09e7609bf26b652c8305002167b
koyakiyo0 <koyakiyo0@gmail.com>
2023-06-13 18:30:51 +0900

    create tab_js

---
+++ tab.js
@@ -0,0 +1,36 @@
+(()=> {
+    
+    const $doc = document;
+    const $tab = $doc.getElementById('js-tab');
+    const $nav = $tab.querySelectorAll('[data-nav]');
+    const $content = $tab.querySelectorAll('[data-content]');
+
+    //初期化
+    const init = () => {
+        $content[0].style.display = 'block';
+    };
+    init();
+
+    //クリックしたら起こるイベント
+    const handleClick = (e) => {
+        e.preventDefault();
+
+        //クリックされたnavとそのdataを取得
+        const $this = e.target;
+        const targetVal = $this.dataset.nav;
+
+
+        console.log('targetval',targetVal );
+
+        //対象のコンテンツをアクティブ化する
+        $tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display= 'block';
+    };
+
+    //全nav要素に対して関数を適応
+    let index = 0;
+    while(index < $nav.length) {
+        $nav[index].addEventListener('click', (e) => handleClick(e));
+        index++;
+    }
+    $nav[0].addEventListener('click', (e) => handleClick(e));
+})();
